11df76d28dfb9b434dfc18a1bed57f2f2cf8a5bf
Bruno Orlandin <brunoorlandin35@gmail.com>
2021-06-23 14:38:31 -0300

    add custom ErrorHandler class to customize errors in the application

---
+++ src/classes/ErrorHandler.ts
@@ -0,0 +1,25 @@
+interface IErrorHandler {
+  name: string,
+  statusCode: number,
+  message: string,
+  description: string
+}
+
+class ErrorHandler {
+  name: string;
+  statusCode: number;
+  message: string;
+  description: string;
+
+
+
+  constructor({ name, statusCode, message, description }: IErrorHandler) {
+    // super(message);
+    this.name = name;
+    this.statusCode = statusCode;
+    this.message = message;
+    this.description = description;
+  }
+}
+
+export { ErrorHandler };
