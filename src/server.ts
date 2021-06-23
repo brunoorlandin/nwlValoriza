84e6ec533d9cc9869c116e4577d2cedf12b3516f
Bruno Orlandin <brunoorlandin35@gmail.com>
2021-06-23 00:03:40 -0300

    aula 3

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,33 @@
-import express from "express";
+import express, { Request, Response, NextFunction, response } from "express";
+import "express-async-errors";
 import "reflect-metadata";
+
 import { router } from "./routes";
 
-const PORT = 3000;
-
 import "./database";
+
+const PORT = 3000;
 
 const app = express();
 
 app.use(express.json());
 
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
